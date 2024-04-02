21663e9fa821e6d56d62c718273ada5e74d3a262
ColeLikesCoding <coledouglasmiller@gmail.com>
2024-04-01 22:05:52 -0700

    eye tracker works LETS GO

--- eye_tracking.js
+++ eye_tracking.js
@@ -10,23 +10,33 @@ window.onresize = (e) => {
 };
 
 document.onmousemove = (e) => {
-    temp.style.left = (window.innerWidth / 2 + 440 * scaleX) + "px";
-    temp.style.top = (window.innerHeight / 2 - 210 * scaleY) + "px";
     var left_dx = e.clientX - (window.innerWidth / 2 + 440 * scale);
     var left_dy = e.clientY - (window.innerHeight / 2 - 210 * scale); // 64
-    var right_dx = e.clientX - (window.innerWidth / 2 + 760 * scale);
+    var right_dx = e.clientX - (window.innerWidth / 2 + 730 * scale);
     var right_dy = e.clientY - (window.innerHeight / 2 - 200 * scale); // 50
-    
-    console.log((e.clientX - window.innerWidth / 2) / scale + " " + (e.clientY - window.innerHeight / 2) / scale);
-    //console.log(left_dx + " " + left_dy + "        " + right_dx + " " + right_dy);
 
     var left_dist = Math.sqrt(left_dx * left_dx + left_dy * left_dy);
     var right_dist = Math.sqrt(right_dx * right_dx + right_dy * right_dy);
 
-    var left_x = 10 + 10 * left_dx / left_dist;
-    var left_y = 10 * left_dy / left_dist;
-    var right_x = 16 + 10 * right_dx / right_dist;
-    var right_y = 2 + 10 * right_dy / right_dist;
-    header.style.backgroundPositionX = "0," + left_x * scale + "px," + right_x * scale + "px,0";
-    header.style.backgroundPositionY = "0," + left_y * scale + "px," + right_y * scale + "px,0";
+    var left_x = 10, left_y = 0;
+    if (left_dist > 10 * scale) {
+        left_x += 10 * left_dx / left_dist;
+        left_y += 10 * left_dy / left_dist;
+    }
+    else {
+        left_x += left_dx / scale;
+        left_y += left_dy / scale;
+    }
+
+    var right_x = 16, right_y = 2;
+    if (right_dist > 10 * scale) {
+        right_x += 10 * right_dx / right_dist;
+        right_y += 10 * right_dy / right_dist;
+    }
+    else {
+        right_x += right_dx / scale;
+        right_y += right_dy / scale;
+    }
+    header.style.backgroundPositionX = `50%, calc(50% + ${left_x * scale}px), calc(50% + ${right_x * scale}px), 50%`;
+    header.style.backgroundPositionY = `50%, calc(50% + ${left_y * scale}px), calc(50% + ${right_y * scale}px), 50%`;
 };
